c49984094ebf962333c4903a22f955460efbb449
dinh hong phi <dinhhongphi@hotmail.com>
2017-03-24 19:59:57 +0700

    security api key. create development.json

--- nodejs/meteor-basic/phi/server/social-config.js
+++ nodejs/meteor-basic/phi/server/social-config.js
@@ -2,8 +2,15 @@ ServiceConfiguration.configurations.remove({
     service: "facebook"
 });
 
-ServiceConfiguration.configurations.insert({
-    service: "facebook",
-    appId: '1260711010711831',
-    secret: 'b1a84065e5f0141ae08522d09f74e251'
-});
+ServiceConfiguration.configurations.upsert(
+    {
+        service: 'facebook'
+    },
+    {
+        $set: {
+            appId: Meteor.settings.facebook.appId,
+            loginStyle: 'popup',
+            secret: Meteor.settings.facebook.secret
+        }
+    }
+);
